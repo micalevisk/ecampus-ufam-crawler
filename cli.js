896c5062ace2d17ca49ee9ca614cf21c267e150b
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-07-07 15:48:24 -0400

    refactor: clear out sensitive data from `process.env`

--- cli.js
+++ cli.js
@@ -7,6 +7,9 @@ if (require.main === module) {
     login: process.env.ECAMPUS_LOGIN,
     password: process.env.ECAMPUS_PASSWORD,
   };
+  // Clear sensititve env. vars. to prevent bad usage from external dependencies.
+  delete process.env.ECAMPUS_LOGIN;
+  delete process.env.ECAMPUS_PASSWORD;
 
   require('./notas-ecampus')(credentials);
 }
